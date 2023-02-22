70eba8f5ffb03a7576a0dfde0c7a718f9aebdc83
Sergii Podobrii <podobri@ukr.net>
2023-02-22 21:56:22 +0200

    Task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryRender = createGallery(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryRender);
+galleryContainer.addEventListener("click", openModal);
+
+function createGallery(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+      </a>
+    </div>
+  `;
+    })
+    .join("");
+}
+
+function openModal(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const imageSrc = event.target.dataset.source;
+  const instance = basicLightbox.create(
+    `<img src='${imageSrc}' width="800" height="600">`
+  );
+
+  instance.show();
+
+  const onEscapePress = (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", onEscapePress);
+    }
+  };
+
+  window.addEventListener("keydown", onEscapePress);
+}
