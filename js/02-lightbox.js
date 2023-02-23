71ad51558b9a4ae295c294ae971314987d7a476a
Sergii Podobrii <podobri@ukr.net>
2023-02-23 14:12:01 +0200

    Task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+const galleryContainer = document.querySelector(".gallery");
+const galleryRender = createGallery(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryRender);
 
-console.log(galleryItems);
+function createGallery(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+ <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
